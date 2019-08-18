e43ca1a068236b8337fccbdf8e78bcbc3f226fbe
Marc Davies <marcdaviesriot@gmail.com>
2019-08-17 22:36:54 -0700

    add superglobals

--- letter.php
+++ letter.php
@@ -1,7 +1,8 @@
 <?php
-$friend_name = "Jennifer";
-$partner_name = "Bob";
-$pet_name = "Fluffles";
+$sender_name = $_GET["sender"];
+$recipient_name = $_GET["recipient"];
+$partner_name = $_GET["partner"];
+$pet_name = $_GET["pet"];
 ?>
 
 <!DOCTYPE html>
@@ -13,19 +14,19 @@ $pet_name = "Fluffles";
 
 <body>
     <h1>Greetings From Afar</h1>
-    <p>Dear <?php echo $friend_name; ?>,</p>
+    <p>Dear <?php echo $recipient_name; ?>,</p>
     <p>
         How are you? How are <?php echo $partner_name; ?> and <?php echo $pet_name; ?>? I hope that you are having a nice weekend. I'm vacationing in
         the mountains of Tibet while I learn programming!
     </p>
     <p>
-        <?php echo $friend_name; ?>, you would not believe how cold it is here. I should have gone to
+        <?php echo $recipient_name; ?>, you would not believe how cold it is here. I should have gone to
         Hawaii instead. But I like programming a lot, so I've got that going for
         me.
     </p>
     <p>Looking forward to seeing you soon. I'll bring you back a souvenir.</p>
     <p>Cheers,</p>
-    <p>Diane</p>
+    <p><?php echo $sender_name; ?></p>
 </body>
 
 </html>
