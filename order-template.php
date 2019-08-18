ff5379f55c84cd09805cd6f93787bbb4d5cfce73
Marc Davies <marcdaviesriot@gmail.com>
2019-08-17 21:21:07 -0700

    add fourth location

--- order-template.php
+++ order-template.php
@@ -3,9 +3,11 @@
     $downtown_address = "208 SW 5th Ave #105, Portland, OR 97204";
     $northwest_address = "1978 19th Ave, Portland, OR 97209";
     $south_address = "1493 SW 2nd Ave, Portland, OR 97201";
+    $west_address = "11735 SW Beaverton Hillsdale Hwy, Beaverton, OR 97005";
     $downtown_order = "300 Cups";
     $northwest_order = "500 Cups";
     $south_order = "1000 Cups";
+    $west_order = "750 Cups";
 ?>
 
 <!DOCTYPE html>
@@ -23,6 +25,8 @@
     <p class = 'address'><?php echo $northwest_address; ?></p>
     <p><?php echo $south_order; ?></p>
     <p class = 'address'><?php echo $south_address; ?></p>
+    <p><?php echo $west_order; ?></p>
+    <p class = 'address'><?php echo $west_address; ?></p>
     <p>Thanks,</p>
     <p>Diane</p>
 </body>
